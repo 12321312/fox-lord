0b4a2f0915a7c44a553795b48cf5415be428fc0a
12321312 <xshadownightx12@gmail.com>
2019-04-11 21:40:32 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,29 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.user.setPresence({
+        game: {
+          name: `Играет в твои нервы`,
+          type: 0  //экспериментируйте доступные значения 0-3, что-то из этого "стримит"
+        }
+      })
+
 client.on('message', message => {
     if (message.content === '!пинг') {
     	message.reply('пшёл нахуй, нормальный пинг');
+  	}
+});
+
+client.on('message', message => {
+    if(message.content === '!тест')
+    if (message.member.roles.equals("537700464888643595"))
+      {      
+        message.reply("Ухх, сюка, сработало!");
+      } 
+    else 
+      {
+        message.reply("Шот не так, сосать");
+      }
   	}
 });
 
