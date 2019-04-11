6b7b64adc1eb609120d4d1b497c5df37162d0048
12321312 <xshadownightx12@gmail.com>
2019-04-12 03:01:47 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,17 +4,7 @@ const vostkl = "!";
 
 client.on('ready', () => {
 console.log('Запущен, сэр!');
-});
-
-bot.on('ready', () => {
-    bot.user.setStatus('available')
-    bot.user.setPresence({
-        game: {
-            name: 'with depression',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
-        }
-    });
+bot.user.setPresence({ game: { name: 'with depression', type: "streaming", url: "https://www.twitch.tv/monstercat"}}); 
 });
 
 client.on('message', message => {
