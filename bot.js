0abe3cc8ed942c38d723dbeb96600231149fbbd7
12321312 <xshadownightx12@gmail.com>
2019-04-11 21:22:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!пинг') {
+    	message.reply('пшёл нахуй, нормальный пинг');
   	}
 });
 
