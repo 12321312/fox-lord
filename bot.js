afcd58c05ee3df69e75704fb963eb28e25e28c28
12321312 <xshadownightx12@gmail.com>
2019-04-12 04:37:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,9 +47,8 @@ client.on('message', message => {
 });
 
 module.exports.run = async (client, message, args) => {
-
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-  if(!args[0]) return message.channel.send("no");
+  if(!message.member.roles.get("537700464888643595")) return message.reply("Не, тебе низя.");
+  if(!args[0]) return message.channel.send("Скок удалять то? =.=");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
