d98c965516f3bb523660b8caabb3af4efaf1f036
12321312 <xshadownightx12@gmail.com>
2019-04-12 04:38:30 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,18 +46,5 @@ client.on('message', message => {
     }
 });
 
-module.exports.run = async (client, message, args) => {
-  if(!message.member.roles.get("537700464888643595")) return message.reply("Не, тебе низя.");
-  if(!args[0]) return message.channel.send("Скок удалять то? =.=");
-  message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
-});
-
-}
-
-module.exports.help = {
-  name: "удалить"
-}
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN); 
