5aadc0bc0d431ced1c476cc442c770a53ee63a25
12321312 <xshadownightx12@gmail.com>
2019-04-11 21:59:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: 'В твои нервы' }, status: 'idle' })
+    client.user.setPresence({ game: { name: 'В твои нервы' }, status: 'Online' })
 });
 
 client.on('message', message => {
