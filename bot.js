ce78e2301dc68373b82e24df42a1112ed02075aa
12321312 <xshadownightx12@gmail.com>
2019-04-12 11:47:31 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ if (msg.startsWith(prefix + 'ОЧИСТИТЬ')) {
                 message.reply('А у кого удалять то? \n Напиши: `' + prefix + 'очистить <@ник> <число>`');
                 return;
        }
-     const fetched = await message.member.channel.fetchMessages({limit: args[0]});
+     const fetched = await message.member.channel.fetchMessages({limit: args[1]});
      console.log(fetched.size + ' сообщения найдены, удаление...'); 
      message.reply('удалено `' + fetched.size + '` сообщений');       
            
