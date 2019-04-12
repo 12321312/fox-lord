824b45f021a0dd0618517da4af268c07650921a6
12321312 <xshadownightx12@gmail.com>
2019-04-12 11:43:00 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,5 +66,40 @@ if (msg.startsWith(prefix + 'УДАЛИТЬ')) {
  }      
 });
 
+client.on('message', message => {
+    let msg = message.content.toUpperCase();
+    let sender = message.author; 
+    let cont = message.content.slice(prefix.length).split(" "); 
+    let args = cont.slice(1); 
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+if (msg.startsWith(prefix + 'ОЧИСТИТЬ')) {
+    async function purge() {
+       if (!message.member.roles.get("537700464888643595")) {
+                message.reply('Вы не можете удалять сообщения. :с'); 
+                return; 
+       }
+       if (isNaN(args[1])) {
+                message.reply('А сколько удалять то? \n Напиши: `' + prefix + 'удалить <число>`');
+                return;
+       }
+       if ((args[1]) >= 100) {
+                message.reply('Больше 100 за раз не могу ;с');
+                return;
+       }
+       if (isNaN(args[0])) {
+                message.reply('А у кого удалять то? \n Напиши: `' + prefix + 'очистить <@ник> <число>`');
+                return;
+       }
+     const fetched = await message.member.channel.fetchMessages({limit: args[0]});
+     console.log(fetched.size + ' сообщения найдены, удаление...'); 
+     message.reply('удалено `' + fetched.size + '` сообщений');       
+           
+     message.channel.bulkDelete(fetched)
+     .catch(error => message.channel.send(`Error: ${error}`));   
+ }
+  purge();
+ }      
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN); 
