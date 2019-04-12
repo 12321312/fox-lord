3db1d8fd91f4625b43b33aff84ea64a4234530fa
12321312 <xshadownightx12@gmail.com>
2019-04-12 10:25:45 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,22 +46,28 @@ client.on('message', message => {
     }
 });
 
-client.on('message', message => {
-  if (message.content === vostkl + 'Удалить') {
+ client.on('message', message => {
+  if (message.content === vostkl + '123') {
   if(!message.member.roles.get("537700464888643595")) {
-         return message.reply("Не, тебе низя.");
-  }
-  if(!args[0]) {
-         return message.channel.send("Скок удалять то? =.=");
   }
   else {
-  message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
-})
   }
 }
+ });
+
+module.exports.run = async (client, message, args) => {
+let args = cont.slice(1);
+  if(!message.member.roles.get("537700464888643595")) return message.reply("No.");
+  if(!args[0]) return message.channel.send("no");
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
 
+}
+
+module.exports.help = {
+  name: "удалить"
+};
 
 
 // THIS  MUST  BE  THIS  WAY
