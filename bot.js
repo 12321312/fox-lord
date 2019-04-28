634d12478eee3136d232248f43996ff254c3d70b
12321312 <xshadownightx12@gmail.com>
2019-04-29 02:29:08 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "!";
-const fs = require("fs");
+const fs = require("fs"); 
 
 fs.readdir("./commands/", (err, files) => {
 
