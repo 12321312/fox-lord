39c61d3d34406b6bd0f3f06362a24af6a88aae32
12321312 <xshadownightx12@gmail.com>
2019-04-29 02:02:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "!";
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection(); 
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -17,7 +15,6 @@ fs.readdir("./commands/", (err, files) => {
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
   });
 });
 
