d88461228e115950e5ffe965b364946c10c66816
12321312 <xshadownightx12@gmail.com>
2019-04-30 04:29:39 +0700

    Create clean.js

---
+++ commands/clean.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "!";
+
+client.on('message', message => {
+    let msg = message.content.toUpperCase();
+    let sender = message.author; 
+    let cont = message.content.slice(prefix.length).split(" "); 
+    let args = cont.slice(1); 
+if (msg.startsWith(prefix + 'УДАЛИТЬ')) {
+    async function purge() {
+       if (!message.member.roles.get("537700464888643595")) {
+                message.reply('Вы не можете удалять сообщения. :с'); 
+                return; 
+       }
+       if (isNaN(args[0])) {
+                message.reply('А сколько удалять то? \n Напиши: `' + prefix + 'удалить <число>`');
+                return;
+       }
+       if ((args[0]) >= 100) {
+                message.reply('Больше 100 за раз не могу ;с');
+                return;
+       }
+     const fetched = await message.channel.fetchMessages({limit: args[0]});
+     console.log(fetched.size + ' сообщения найдены, удаление...'); 
+     message.reply('удалено `' + fetched.size + '` сообщений');       
+           
+     message.channel.bulkDelete(fetched)
+     .catch(error => message.channel.send(`Error: ${error}`));   
+ }
+  purge();
+ }                
+});
