2700661733f96d5b3d8a0713846bb1aeaf9bd12a
12321312 <xshadownightx12@gmail.com>
2019-04-30 04:30:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,52 +30,6 @@ client.user.setPresence({
    })
 });
 
-client.on('message', message => {
-    let msg = message.content.toUpperCase();
-    let sender = message.author; 
-    let cont = message.content.slice(prefix.length).split(" "); 
-    let args = cont.slice(1); 
-if (msg.startsWith(prefix + 'УДАЛИТЬ')) {
-    async function purge() {
-       if (!message.member.roles.get("537700464888643595")) {
-                message.reply('Вы не можете удалять сообщения. :с'); 
-                return; 
-       }
-       if (isNaN(args[0])) {
-                message.reply('А сколько удалять то? \n Напиши: `' + prefix + 'удалить <число>`');
-                return;
-       }
-       if ((args[0]) >= 100) {
-                message.reply('Больше 100 за раз не могу ;с');
-                return;
-       }
-     const fetched = await message.channel.fetchMessages({limit: args[0]});
-     console.log(fetched.size + ' сообщения найдены, удаление...'); 
-     message.reply('удалено `' + fetched.size + '` сообщений');       
-           
-     message.channel.bulkDelete(fetched)
-     .catch(error => message.channel.send(`Error: ${error}`));   
- }
-  purge();
- }                
-});
-
-client.on('guildMemberAdd', member => {
-  console.log('User ' + member.user.tag + ' зашёл на сервер!');
-  let channel = client.channels.get("537720268446236682");
-  var role = member.guild.roles.get("537701217879588878");
-  let esyy = client.emojis.get("554122910584012800");
-  channel.send("На сервер зашёл **"+member.user.tag+"**! "+`${esyy}`);
-  member.addRole(role);
-});
-
-client.on('guildMemberRemove', member => {
-  console.log('User ' + member.user.tag + ' вышел с сервера!');
-  let channel = client.channels.get("537720268446236682");
-  let nsyy = client.emojis.get("554122783165251585");
-  channel.send("**"+member.user.tag+"** вышел с сервера! "+`${nsyy}`);
-});
-
  
 // THIS  MUST  BE  THIS  WAY 
 client.login(process.env.BOT_TOKEN); 
