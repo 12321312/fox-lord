3d3b784b37fd1b6cf04d174b39aa882583dfc342
12321312 <xshadownightx12@gmail.com>
2019-04-30 04:28:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,20 +29,6 @@ client.user.setPresence({
        }
    })
 });
- 
-
-client.on('message', message => {
-  if (message.content === prefix + 'лав') {
-  message.author.sendMessage("Я люблю тебя ♥");
-  message.delete(1);
-  }
-});
-
-client.on('message', message => {
-    if (message.content === prefix + 'пинг') {
-    message.reply('да нормальный у тебя пинг `' + `${message.createdTimestamp - Date.now()}` + '` м/с, успокойся');
-  	}
-});
 
 client.on('message', message => {
     let msg = message.content.toUpperCase();
@@ -73,36 +59,6 @@ if (msg.startsWith(prefix + 'УДАЛИТЬ')) {
   purge();
  }                
 });
-
-client.on('message', message => {
-    let msg = message.content.toUpperCase();
-    let cont = message.content.slice(prefix.length).split(" "); 
-    let args = cont.slice(1); 
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);   
-  if (msg.startsWith(prefix + 'Д')) {
-  if (message.author.id !== "294844223675564034") {
-  message.reply('Хитрожопых наказываю'); 
-  return;
-  }
-  message.delete();
-  message.channel.send(member + ' ' + args[1]);
-  }
-});
-
-client.on('message', message => {
-    let msg = message.content.toUpperCase();
-    let cont = message.content.slice(prefix.length).split(" "); 
-    let args = cont.slice(1); 
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);   
-  if (msg.startsWith(prefix + 'Л')) {
-  if (message.author.id !== "294844223675564034") { 
-  message.reply('Хитрожопых наказываю'); 
-  return;
-  }
-  message.delete();
-  member.sendMessage(args[1]);
-  }
-}); 
 
 client.on('guildMemberAdd', member => {
   console.log('User ' + member.user.tag + ' зашёл на сервер!');
