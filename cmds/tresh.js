0a5d0245026704a7fa3894100a9ccb0e01edd0a5
12321312 <xshadownightx12@gmail.com>
2019-05-01 05:56:31 +0700

    Update tresh.js

--- cmds/tresh.js
+++ cmds/tresh.js
@@ -1,8 +1,8 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    message.channel.send('pong!');
+    message.reply('да нормальный у тебя пинг `' + `${message.createdTimestamp - Date.now()}` + '` м/с, успокойся');
 };
 module.exports.help = {
-    name: "треш"
+    name: "пинг"
 };
