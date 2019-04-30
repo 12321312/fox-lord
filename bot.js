eb85213a52785ae85e11d7a3338da52a262ff52c
12321312 <xshadownightx12@gmail.com>
2019-04-30 07:23:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,20 +49,50 @@ bot.on("message", async message => {
 
 });
 
-client.on('message', async message => {
-  if (message.content === prefix + 'инфо') {
-    
-    const sicon = message.guild.iconURL;
-    const pipiska = new Discord.RichEmbed()
-    .setDescription("Информация сервера:")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Название сервера:", message.guild.name)
-    .addField("Создан:", message.guild.createdAt)
-    .addField("Вы зашли:", message.member.joinedAt)
-    .addField("Всего участников:", message.guild.memberCount);
-    
-    return message.channel.send({pipiska});
+client.on('message', message => {
+  if (message.content === prefix + 'инфо') { 
+    message.channel.send({pipiska:
+    {
+  "embed": {
+    "title": "Добро пожаловать на сервер **Fox Shelter**!",
+    "description": "Сервер был специально создан для разностных лиц, которые увлекаются разными вещами.",
+    "url": "https://discordapp.com",
+    "color": 4680435,
+    "timestamp": "2019-04-30T00:14:38.627Z",
+    "footer": {
+      "icon_url": "https://pp.userapi.com/FaynRO8qPqBAaCDWK9OBhIPbmmu2n2oAI6xfuw/UksmaVfOhd4.jpg?ava=1",
+      "text": "Не нарушай педрила"
+    },
+    "thumbnail": {
+      "url": "https://static.tgstat.ru/public/images/channels/_0/2a/2a29043c84a2419fe23a5895ca3f24d8.jpg"
+    },
+    "image": {
+      "url": ""
+    },
+    "author": {
+      "name": "LousyFox.:з",
+      "url": "https://discordapp.com",
+      "icon_url": "https://yt3.ggpht.com/a-/AAuE7mAHy4ulOOlJr8f6za5LTbCqhy5CsWGi6mIrZQ=s900-mo-c-c0xffffffff-rj-k-no"
+    },
+    "fields": [
+      {
+        "name": "🤔",
+        "value": "Вы вон там, справа..."
+      },
+      {
+        "name": "😀",
+        "value": "За хорошое поведение плюшки",
+        "inline": true
+      },
+      {
+        "name": "😠",
+        "value": "За плохое поведение пизды",
+        "inline": true
+      }
+    ]
+  }
+}                     
+                         });
   }
 });
 
