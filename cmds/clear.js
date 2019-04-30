76dfd017590a51b3143de2c76937fdae8ccc2ac7
12321312 <xshadownightx12@gmail.com>
2019-05-01 06:11:51 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
 
 if (!message.member.roles.get("537700464888643595")) { message.reply('Вы не можете удалять сообщения. :с'); return; }
-if (isNaN(args[0])) {message.reply('А сколько удалять то? \n Напиши: `' + prefix + 'удалить <число>`'); return; }
+if (isNaN(args[0])) {message.reply('А сколько удалять то? \n Напиши: `!удалить <число>`'); return; }
 if ((args[0]) >= 100) { message.reply('Больше 100 за раз не могу ;с'); return; }
 
 const fetched = await message.channel.fetchMessages({limit: args[0]});
