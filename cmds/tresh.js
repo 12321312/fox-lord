5bec30ed8178ca796541cd1554f24559e60bdf55
12321312 <xshadownightx12@gmail.com>
2019-05-01 05:46:38 +0700

    Update tresh.js

--- cmds/tresh.js
+++ cmds/tresh.js
@@ -1,6 +1,6 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
-module.exports.run = async (bot,message,args) => {
+module.exports.run = async (client,message,args) => {
     message.channel.send('pong!');
 };
 module.exports.help = {
