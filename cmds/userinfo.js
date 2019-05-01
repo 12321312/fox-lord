ea2e7bad095d313332b9b342cf1e4c8aa9e8e915
12321312 <xshadownightx12@gmail.com>
2019-05-01 10:46:26 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -2,7 +2,15 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     let a = message.author;
-    let ambed = new Discord.RichEmbed().setDescription("Информация о сервере").setColor('#10c7e2').addField("Имя",a.username).addField("Тэг",a.tag).addField("Дискриминатор",a.discriminator).addField("Создание аккаунта",a.createdAt).addField("ID",a.id).addField("Вы бот?",a.bot).setThumbnail(a.avatarURL);
+    let ambed = new Discord.RichEmbed()
+    .setTitle("Информация о участнике")
+    .setTimestamp()
+    .setFooter("Твой милый бот", bot.avatarURL)
+    .setColor('#10c7e2').addField("Имя",a.username)
+    .addField("Тэг",a.tag)
+    .addField("Дискриминатор",a.discriminator)
+    .addField("Создание аккаунта",a.createdAt)
+    .setThumbnail(a.avatarURL);
 
     bot.send({embed:ambed});
 
