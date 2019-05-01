5e1b5ebb24158fb4db7241bea30df3fc3af6d98c
12321312 <xshadownightx12@gmail.com>
2019-05-01 07:17:04 +0700

    Create ls.js

---
+++ cmds/ls.js
@@ -0,0 +1,17 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);  
+    let botmessage = args.join((args[0])+" ");
+    if (message.author.id !== "294844223675564034") { message.reply('Хитрожопых наказываю'); return; }
+    if ((args[0])==null && (args[1])==null) { message.delete(); message.reply('пусто везде бл...'); return; }
+    if ((args[0])==null) { message.delete(); message.reply('Пустота в обращении...'); return; }
+    if (!member) { message.delete(); message.reply('Такого нету...'); return; }
+    if ((args[1])==null) { message.delete(); message.reply('Пустота в описании...'); return; }
+
+    message.delete();
+    member.sendMessage(botmessage);
+};
+module.exports.help = {
+    name: "лс"
+};
