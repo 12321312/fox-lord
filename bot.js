92abf553a5ffee947c59334fa3b7133656b7b8ae
12321312 <xshadownightx12@gmail.com>
2019-05-01 11:43:50 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,88 +1,103 @@
-const Discord = module.require("discord.js");
-const fs = require("fs");
-module.exports.run = async (bot,message,args) => {
-    await message.delete().catch(O_o=>{});
+const Discord = require("discord.js");
+const botconfig = require("./config.json");
+const bot = new Discord.Client();
+const fs = require('fs');
+bot.commands = new Discord.Collection();
+let config = require('./config.json');
+let prefix = config.prefix;
+let profile = require('./profile.json');
 
-    const a = message.guild.roles.get('537706487842340865'); // дота
-    const b = message.guild.roles.get('537706608105619457'); // гмод
-    const c = message.guild.roles.get('537706571015258156'); // Eve
+// подключение
+fs.readdir('./cmds/',(err,files)=>{
+  if(err) console.log(err);
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+  if(jsfiles.length <=0) console.log("Нет комманд для загрузки!!");
+  console.log(`Загружено ${jsfiles.length} комманд`);
+  jsfiles.forEach((f,i) =>{
+      let props = require(`./cmds/${f}`);
+      console.log(`${i+1}.${f} Загружен!`);
+      bot.commands.set(props.help.name,props);
+  });
+});
 
-    let dota = bot.emojis.get("573000975250489345");
-    let gmod = bot.emojis.get("573000973367246849");
-    let eve = bot.emojis.get("573000974503772172");
+// проверка текста
+bot.on('message', async message => {
+  if(message.author.bot) return;
+  if(message.channel.type == "dm") return;
+  let user = message.author.username;
+  let uid = message.author.id;
+  bot.send = function (msg){
+        message.channel.send(msg);
+  };
+  if(!profile[uid]){
+    profile[uid] ={
+        coins:10,
+        warns:0,
+        xp:0,
+        lvl:1,
+    };
+  };
+  let u = profile[uid];
 
-    const filter = (reaction, user) => ['573000975250489345', '573000973367246849', '573000974503772172'].includes(reaction.emoji.get) && user.id === message.author.id;
+  u.coins++;
+  u.xp++;
 
-    let ombed = new Discord.RichEmbed()
-        .setTitle('Выберите ключ')
-        .setDescription(`
-        ${dota} ${a.toString()} 
-        ${gmod} ${b.toString()} 
-        ${eve} ${c.toString()}
-        `)
-        .setColor(0xdd9323)
-        .setFooter(`ID: ${message.author.id}`);
-        
-    message.channel.send({embed:ombed}).then(async msg => {
+  if(u.xp>= (u.lvl * 5)){
+      u.xp = 0;
+      u.lvl += 1;
+  };
 
-        await msg.react('573000975250489345');
-        await msg.react('573000973367246849');
-        await msg.react('573000974503772172');
+  fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
+    if(err) console.log(err);
+  });
 
-        msg.awaitReactions(filter, {
-            max: 1,
-            time: 30000,
-            errors: ['time']
-        }).then(collected => {
+  let messageArray = message.content.split(" ");
+  let command = messageArray[0].toLowerCase();
+  let args = messageArray.slice(1);
+  if(!message.content.startsWith(prefix)) return;
+  let cmd = bot.commands.get(command.slice(prefix.length));
+  if(cmd) cmd.run(bot,message,args);
+});
 
-            const reaction = collected.first();
+// шапка
+bot.on('ready', () => {
+  console.log('Запущен, сэр!');
+  bot.user.setPresence({
+         status: "online",
+         game: {
+             name: "твои нервы",
+             url: "https://www.youtube.com/watch?v=qrohU75OdJ8",
+             type: "STREAMING"
+         }
+     });
+});
 
-            switch (reaction.emoji.get) {
-                case '573000975250489345':
-                    if (message.member.roles.has(a.id)) {
-                        msg.delete(2000);
-                        return message.channel.send('Вы уже имеете этот ключ!').then(m => m.delete(3000));
-                    }
-                    message.member.addRole(a).catch(err => {
-                        console.log(err);
-                        return message.channel.send(`Ошибка выдачи ключа: **${err.message}**.`);
-                    });
-                    message.channel.send(`Вам был выдан **${a.name}**!`).then(m => m.delete(3000));
-                    msg.delete();
-                    break;
-                case '573000973367246849':
-                    if (message.member.roles.has(b.id)) {
-                        msg.delete(2000);
-                        return message.channel.send('Вы уже имеете этот ключ!').then(m => m.delete(3000));
-                    }
-                    message.member.addRole(b).catch(err => {
-                        console.log(err);
-                        return message.channel.send(`Ошибка выдачи ключа: **${err.message}**.`);
-                    });
-                    message.channel.send(`Вам был выдан **${b.name}**!`).then(m => m.delete(3000));
-                    msg.delete();
-                    break;
-                case '573000974503772172':
-                    if (message.member.roles.has(c.id)) {
-                        msg.delete(2000);
-                        return message.channel.send('Вы уже имеете этот ключ!').then(m => m.delete(3000));
-                    }
-                    message.member.addRole(c).catch(err => {
-                        console.log(err);
-                        return message.channel.send(`Ошибка выдачи ключа: **${err.message}**.`);
-                    });
-                    message.channel.send(`Вам был выдан **${c.name}**!`).then(m => m.delete(3000));
-                    msg.delete();
-                    break;
-            }
-        }).catch(collected => {
-            return message.channel.send(`Я не смогу вас добавить :с`);
-        });
+// Автороль
+bot.on('guildMemberAdd', member => {
+  console.log('User ' + member.user.tag + ' зашёл на сервер!');
+  let channel = bot.channels.get("537720268446236682");
+  var role = member.guild.roles.get("537701217879588878");
+  let esyy = bot.emojis.get("554122910584012800");
+  channel.send("На сервер зашёл **"+member.user.tag+"**! "+`${esyy}`);
+  member.addRole(role);
+});
 
-    });
+bot.on('guildMemberRemove', member => {
+  console.log('User ' + member.user.tag + ' вышел с сервера!');
+  let channel = bot.channels.get("537720268446236682");
+  let nsyy = bot.emojis.get("554122783165251585");
+  channel.send("**"+member.user.tag+"** вышел с сервера! "+`${nsyy}`);
+});
 
-};
-
-module.exports.help = {
-    name: 'ключ'
-};
+// Автореакция 
+bot.on('message', (receivedMessage) => {
+    if (receivedMessage.author == bot.user) {
+        return
+    }
+    if (receivedMessage.content.indexOf(prefix) == 0) {
+    receivedMessage.react(bot.emojis.get("554122910584012800"))
+    }
+});
+  
+// login
+bot.login(process.env.BOT_TOKEN);
