d14a48dde613a67a292424974d33362d02a62f96
12321312 <xshadownightx12@gmail.com>
2019-05-02 11:41:48 +0700

    Create belayakoshka.js

---
+++ cmds/belayakoshka.js
@@ -0,0 +1,13 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    let botmessage = args.join(" ");
+    message.delete();
+    message.channel.send(botmessage, {
+        file: "./img/lol.png" 
+    });
+};
+
+module.exports.help = {
+    name: 'бк'
+};
