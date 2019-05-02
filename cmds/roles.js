9ffe750b794f10317d54650a46f959bb0faeeb6c
12321312 <xshadownightx12@gmail.com>
2019-05-02 11:38:25 +0700

    Update roles.js

--- cmds/roles.js
+++ cmds/roles.js
@@ -2,6 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     let botmessage = args.join(" ");
+    message.delete();
     message.channel.send(botmessage, {
         file: "./img/lol.png" 
     });
