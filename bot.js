341fe475892e399373e495985ae671677a651c8b
12321312 <xshadownightx12@gmail.com>
2019-05-03 22:12:32 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,8 +108,8 @@ React to the emoji that matches the role you wish to receive.
 If you would like to remove the role, simply remove your reaction!
 `;
 const embedFooter = "Role Reactions"; // Must set this if "embed" is set to true
-const roles = ["Hacker", "Artist", "Public Relations", "Intern"];
-const reactions = ["💻", "🖌", "😃", "🆕"];
+const roles = ["Dota-key", "EVE-key", "Gmod-key", "Secret-key"];
+const reactions = [":dota:", "🖌", "😃", "🆕"];
 const embed = true; // Set to "true" if you want all roles to be in a single embed
 const embedColor = "#dd2423"; // Set the embed color if the "embed" variable is set to true
 
