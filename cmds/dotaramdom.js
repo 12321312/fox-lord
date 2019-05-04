6409a409740574d86e969a754c42167653775906
12321312 <xshadownightx12@gmail.com>
2019-05-04 20:43:22 +0700

    Update dotaramdom.js

--- cmds/dotaramdom.js
+++ cmds/dotaramdom.js
@@ -1,7 +1,8 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
- var Hero = Array('дота1','дота2','дота3')
+    
+ var Hero = Array('дота1','дота2','дота3');
  const randomHero = Hero[Math.floor(Math.random()*Hero.length)];
  let a = message.author;
  let dotasend = new Discord.RichEmbed()
