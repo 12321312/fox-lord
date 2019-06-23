482d31807354f0a116c333a28ed4a3a7e8f42da0
12321312 <xshadownightx12@gmail.com>
2019-06-23 18:53:05 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     let a = message.author;
     let ambed = new Discord.RichEmbed()
-    .setTitle("Команды")
+    .setTitle("Команды:")
     .setTimestamp()
     .setFooter("Твой милый бот", "https://cs4.pikabu.ru/post_img/big/2016/07/16/9/1468678258134342020.jpg")
     .setColor('#FFFF00')
@@ -13,7 +13,8 @@ module.exports.run = async (bot,message,args) => {
     .addField("!инфо","Показывает информацию о сервере")
     .addField("!пидор","Уточняет вашу оринтацию благодоря новым методам, после чего выдает вам роль")
     .addField("!дота","Выдает вам роль, героя и позицию на следующую катку в доте")
-
+    .setThumbnail("http://pngimg.com/uploads/question_mark/question_mark_PNG73.png")
+    
     bot.send({embed:ambed});
 
 };
