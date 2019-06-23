2b0331712a284c2f2a95cc1b0d0dcbf05f3feaa6
12321312 <xshadownightx12@gmail.com>
2019-06-23 22:35:08 +0700

    Create random.js

---
+++ cmds/random.js
@@ -0,0 +1,27 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    let randomfot = args[0];
+    var ramdomcat = Array("кот","мяу","мур","Кот","кошка","котэ","котейка","котёнок","cat","cate","неко","мурка","мур","Котэ","Котейка","Котёнок","Кошечка","кошечка","коты","котейки","Кошечки");
+    var ramdomdog = Array("собака","Собака","Собакин","Dog","Doge","Гаф","гаф","пёс","пес","щенок","шенок","псина");
+    let a = message.author;
+
+    if (randomfot != ramdomcat || randomfot != ramdomdog) { message.reply('Хуй знает что тебе надо'); return; }
+    if (randomfot == ramdomcat) { let inforandom = "http://aws.random.cat/meow"; };  
+    if (randomfot == ramdomcat) { let inforandom = "https://dog.ceo/api/breeds/image/random"; };  
+    let {body} = await superagent
+    .get('inforandom'); 
+
+    let ambed = new Discord.RichEmbed()
+    .setTitle("Мимими")
+    .setTimestamp()
+    .setFooter("Твой милый бот", "https://cs4.pikabu.ru/post_img/big/2016/07/16/9/1468678258134342020.jpg")
+    .setColor('#FFCBDB')
+    .setImage(body.url);
+
+    bot.send({embed:ambed});
+
+};
+module.exports.help = {
+    name: "рандом"
+};
