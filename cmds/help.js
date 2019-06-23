084d526f3d77189b22fbe57140f17e62dbe310cc
12321312 <xshadownightx12@gmail.com>
2019-06-23 19:54:29 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -7,6 +7,7 @@ module.exports.run = async (bot,message,args) => {
     .setTimestamp()
     .setFooter("Твой милый бот", "https://cs4.pikabu.ru/post_img/big/2016/07/16/9/1468678258134342020.jpg")
     .setColor('#FFFF00')
+    .addField("!хелп","Вызывает это окно")
     .addField("!пинг","Устанавливает точное время соединение клианта с сервером")
     .addField("!ролл (цифра)","выдает рандомное число из нужного диапозона")
     .addField("!юзеринфо","Показывает всю информацию о клиенте")
@@ -14,7 +15,7 @@ module.exports.run = async (bot,message,args) => {
     .addField("!пидор","Уточняет вашу оринтацию благодоря новым методам, после чего выдает вам роль")
     .addField("!дота","Выдает вам роль, героя и позицию на следующую катку в доте")
     .setThumbnail("http://pngimg.com/uploads/question_mark/question_mark_PNG73.png")
-    
+
     bot.send({embed:ambed});
 
 };
