dfe8255324d6cacce71fe996949abf4a0bfabdc4
12321312 <xshadownightx12@gmail.com>
2019-06-23 23:00:15 +0700

    Create report.js

---
+++ cmds/report.js
@@ -0,0 +1,32 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    if(args[0] == "help"){
+      message.reply("Не правильная жалоба, напиши так: ```!report <юзер упоминание> <причина>```");
+      return;
+    }
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Такого участника нету, иди нахуй.");
+    let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Репорт")
+    .setColor(orange)
+    .addField("На:", `${rUser} смотреть ID: ${rUser.id}`)
+    .addField("От:", `${message.author} Смотреть ID: ${message.author.id}`)
+    .addField("Канал:", message.channel)
+    .addField("Время:", message.createdAt)
+    .addField("Причина:", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "🏴репорты");
+    if(!reportschannel) return message.channel.send("Нет такого канала.");
+
+
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
+
+}
+
+module.exports.help = {
+  name: "Репорт"
+}
