2856b87a9f609cef4bcecf9884875eaac7cfcc57
12321312 <xshadownightx12@gmail.com>
2019-06-24 22:31:06 +0700

    Update tempmute.js

--- cmds/tempmute.js
+++ cmds/tempmute.js
@@ -26,11 +26,25 @@ let muterole = message.guild.roles.find('name', "muted");
       }
   }
 
+let mutechannel = message.guild.channels.get("537720268446236682");
+if(!mutechannel) return message.channel.send("Сбились настройки логирования, проверьте пожалуйста их.");
 let mutetime = args[1];
 if(!mutetime) return message.reply("Укажите время, блэать!");
+let muteEmbed = new Discord.RichEmbed()
+.setDescription("Мут")
+.setColor('#00538A')
+.setThumbnail("https://freepngimg.com/thumb/silence/7-2-silence-picture-thumb.png")
+.setFooter("Мут систем v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
+.addField("Был замучен:", `${tomute}`)
+.addField("Администратор:", `${message.author}`)
+.addField("Канал:", message.channel)
+.addField("Время мута:", `${ms(ms(mutetime))}`)
+.addField("Начало мута:", message.createdAt);
 
 await(tomute.addRole(muterole.id));
-message.reply(`<@${tomute.id}> был замучен на ${ms(ms(mutetime))}`);
+message.delete();
+message.channel.send('пользователь' + `<@${tomute.id}>` + ' был замучен на`'+ `${ms(ms(mutetime))}` + '`');
+mutechannel.send({embed:muteEmbed});
 
 setTimeout(function(){
     tomute.removeRole(muterole.id);
