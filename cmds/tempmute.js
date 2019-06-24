c27571b8403c4e9b1286c3a2bcb21a277db1ae24
12321312 <xshadownightx12@gmail.com>
2019-06-24 23:11:38 +0700

    Update tempmute.js

--- cmds/tempmute.js
+++ cmds/tempmute.js
@@ -9,7 +9,7 @@ let tomute = message.guild.member(message.mentions.users.first() || message.guil
 if(!tomute) return message.reply("такого участника нету");
 if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("у вас нет прав на это, идите нахуй");
 let muterole = message.guild.roles.find('name', "muted");
-let mreason = args.join(" ").slice(27) || "---";
+let mreason = args[2].join(" ").slice(1) || "---";
 
   if(!muterole){
       try{
