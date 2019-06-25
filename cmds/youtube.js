c575b07a34922c8f2950661d3aa3896dc3771405
12321312 <xshadownightx12@gmail.com>
2019-06-25 19:20:09 +0700

    Update youtube.js

--- cmds/youtube.js
+++ cmds/youtube.js
@@ -2,6 +2,13 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
 
+    async function searchYouTubeAsync(args) {
+        var video = await youtube.searchVideos(args.toString().replace(/,/g,' '));
+        console.log(video.url);
+        console.log(typeof String(video.url));
+        return String(video.url);
+      }
+
 if ((args[0]) == null) { message.reply("так, а чё искать то тебе?"); return;}
 let video = await searchYouTubeAsync(args);
 message.reply(video.url); 
