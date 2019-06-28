ea1735f6c20f7fd06bd8f6d26a85c3deec453ccd
12321312 <xshadownightx12@gmail.com>
2019-06-29 04:19:58 +0700

    Update chlen.js

--- cmds/chlen.js
+++ cmds/chlen.js
@@ -16,7 +16,8 @@ let clien = new Discord.RichEmbed()
 .addField(nameclien, "Линейка вам больше не нужна.")
 .setThumbnail("http://pngimg.com/uploads/ruler/ruler_PNG22.png");
 //var arrclien = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"];
-if (message.author.roles.find('name', `Размер члена ${Infinity}`)) { message.reply('Ваш размер уже определенён был...'); return; }
+var x = Infinity;
+if (message.author.roles.find('name', `Размер члена ${x}`)) { message.reply('Ваш размер уже определенён был...'); return; }
 let clienrole = message.guild.roles.find('name', `Член ${randomclien} см`);
 if(!clienrole){
     try{
