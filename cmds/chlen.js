a26ae2a69e8a754f58fd0a77ed28021ac672b523
12321312 <xshadownightx12@gmail.com>
2019-06-29 04:34:15 +0700

    Update chlen.js

--- cmds/chlen.js
+++ cmds/chlen.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
 let randomclien = Math.floor(Math.random() * 30) + 1 ;
-//if (message.author.id !== "294844223675564034") randomclien = "999999999";
+if (message.author.id !== "294844223675564034") randomclien = "999999999";
 let nameclien = "Уууу блядь п@здец..."; 
 if (randomclien < 25) nameclien = "Ну перед пацанами уже можно выебнуться..."; 
 if (randomclien < 20) nameclien = "Ну сойдет чтобы похвастаться перед скромной тёлкой..."; 
@@ -17,13 +17,13 @@ let clien = new Discord.RichEmbed()
 .setThumbnail("http://pngimg.com/uploads/ruler/ruler_PNG22.png");
 var sizepenis = 0;
 for (sizepenis = 0; sizepenis < 31; sizepenis++) {
-if (message.member.roles.find('name', `Член ${sizepenis} см`)) return message.reply('Ваш размер уже определенён был...');
+if (message.member.roles.find('name', `${sizepenis} см`)) return message.reply('Ваш размер уже определенён был...');
 };
-let clienrole = message.guild.roles.find('name', `Член ${randomclien} см`);
+let clienrole = message.guild.roles.find('name', `${randomclien} см`);
 if(!clienrole){
     try{
         clienrole = await message.guild.createRole({
-            name:`Член ${randomclien} см`,
+            name:`${randomclien} см`,
             color: "#FFCBDB",
             permission: []
         })
