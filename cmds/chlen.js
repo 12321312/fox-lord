3767aff06a9bc9b8bfdf3a8573a870111da89455
12321312 <xshadownightx12@gmail.com>
2019-06-29 03:39:37 +0700

    Update chlen.js

--- cmds/chlen.js
+++ cmds/chlen.js
@@ -4,14 +4,14 @@ module.exports.run = async (bot,message,args) => {
 let randomclien = Math.floor(Math.random() * 30) + 1 ;
 //if (message.author.id !== "294844223675564034") randomclien = "over999";
 let nameclien = "Уууу блядь п@здец..."; 
-if (randomclien > 15) nameclien = "у вас пиздец маленький, советую не показывать девочкам, описаются от смеха..."; 
-if (randomclien > 25) nameclien = "ну сойдет чтобы похвастаться перед скромной тёлкой..."; 
+if (randomclien < 15) nameclien = "у вас пиздец маленький, советую не показывать девочкам, описаются от смеха..."; 
+if (randomclien < 25) nameclien = "ну сойдет чтобы похвастаться перед скромной тёлкой..."; 
 let clien = new Discord.RichEmbed()
 .setTitle(message.author.username)
 .setTimestamp()
 .setFooter("Измеритель пениса v228", "https://icon2.kisspng.com/20180425/zuw/kisspng-youtube-video-animation-tenor-5ae0d876161ae1.9363982315246849180906.jpg")
 .setColor('#964B00')
-.addField("Размер вашего члена состоявляет", `\`\`\`js\n${randomclien}+"см"+\`\`\``)
+.addField("Размер вашего члена состоявляет", `\`\`\`js\n${randomclien} см\`\`\``)
 .addField(nameclien, "линейка вам больше не нужна.")
 .setThumbnail("http://pngimg.com/uploads/ruler/ruler_PNG22.png");
 
