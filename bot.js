44d116f1377ba3f2d8f3428c58258a5bdbfbb06f
12321312 <xshadownightx12@gmail.com>
2019-07-04 04:20:32 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,12 +20,12 @@ antispam(bot, {
   warnBuffer: 3,
   maxBuffer: 5,
   interval: 1000, 
-  warningMessage: "хватит спамить!.", 
-  roleMessage: "Замучен!!", 
+  warningMessage: "хватит спамить!", 
+  roleMessage: "В мут за спам!", 
   roleName: "muted", 
   maxDuplicatesWarning: 7, 
   maxDuplicatesBan: 10, 
-  time: 10, 
+  time: 6000, 
 });
 // бот реакции
 if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
