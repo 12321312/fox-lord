597104a1201a7760f74a4b3248fe00153228530c
12321312 <xshadownightx12@gmail.com>
2019-07-04 19:01:59 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,8 +1,8 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args,connection) => {
-    let target = message.author;
-    connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
+    let a = message.author;
+    connection.query(`SELECT * FROM xp WHERE id = '${a.id}'`, (err, rows) => {
      if(err) throw err;
      let xpi = rows[0].xp;
      let point = rows[0].point;
@@ -16,14 +16,14 @@ module.exports.run = async (bot,message,args,connection) => {
     .setTitle("Информация о участнике")
     .setTimestamp()
     .setFooter("Твой милый бот", "https://cs4.pikabu.ru/post_img/big/2016/07/16/9/1468678258134342020.jpg")
-    .setColor('#10c7e2').addField("Имя",target.username)
-    .addField("Тэг",target.tag)
-    .addField("Дискриминатор",target.discriminator)
+    .setColor('#10c7e2').addField("Имя",a.username)
+    .addField("Тэг",a.tag)
+    .addField("Дискриминатор",a.discriminator)
     .addField("Опыта:",xpi + " XP")
     .addField("Уровень:",lvl)
     .addField("Донат поинтов:",point)
-    .addField("Создание аккаунта",target.createdAt)
-    .setThumbnail(target.avatarURL);
+    .addField("Создание аккаунта",a.createdAt)
+    .setThumbnail(a.avatarURL);
 
     bot.send({embed:ambed});
 });
