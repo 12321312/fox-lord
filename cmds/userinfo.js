96bfb6d3337f9ddddb6975445aca9f3ab1ebaae6
12321312 <xshadownightx12@gmail.com>
2019-07-04 14:24:35 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,20 +1,19 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args,connection) => {
-    let target = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]) || message.author);
+    let target = message.author;
     connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
      if(err) throw err;
      let xp = rows[0].xp;
-    let a = message.author;
     let ambed = new Discord.RichEmbed()
     .setTitle("Информация о участнике")
     .setTimestamp()
     .setFooter("Твой милый бот", "https://cs4.pikabu.ru/post_img/big/2016/07/16/9/1468678258134342020.jpg")
-    .setColor('#10c7e2').addField("Имя",a.username)
-    .addField("Тэг",a.tag)
-    .addField("Дискриминатор",a.discriminator)
+    .setColor('#10c7e2').addField("Имя",target.username)
+    .addField("Тэг",target.tag)
+    .addField("Дискриминатор",target.discriminator)
     .addField("Опыта:",xp)
-    .addField("Создание аккаунта",a.createdAt)
+    .addField("Создание аккаунта",target.createdAt)
     .setThumbnail(a.avatarURL);
 
     bot.send({embed:ambed});
