a46ed80903b4b71c0c8655cde78d5b00baa5c27a
12321312 <xshadownightx12@gmail.com>
2019-07-04 19:09:38 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot,message,args,connection) => {
      //let point = rows[0].point;
     
     var lvl = 1;
-    if (xp > 1000) lvl = 1; if (xp > 2000) lvl = 2; if (xp > 3000) lvl = 3; if (xp > 4000) lvl = 4; if (xp > 5000) lvl = 5; if (xp > 7000) lvl = 7; if (xp > 8000) lvl = 8; if (xp > 9000) lvl = 9; if (xp > 10000) lvl = 10; if (xp > 11000) lvl = 11; if (xp > 12000) lvl = 12; if (xp > 13000) lvl = 13; if (xp > 14000) lvl = 14; if (xp > 15000) lvl = 15; if (xp > 16000) lvl = 16;
-    if (xp > 17000) lvl = 17; if (xp > 18000) lvl = 18; if (xp > 19000) lvl = 19; if (xp > 20000) lvl = 20;
+    if (xpi > 1000) lvl = 1; if (xpi > 2000) lvl = 2; if (xpi > 3000) lvl = 3; if (xpi > 4000) lvl = 4; if (xpi > 5000) lvl = 5; if (xpi > 7000) lvl = 7; if (xpi > 8000) lvl = 8; if (xpi > 9000) lvl = 9; if (xpi > 10000) lvl = 10; if (xpi > 11000) lvl = 11; if (xpi > 12000) lvl = 12; if (xpi > 13000) lvl = 13; if (xpi > 14000) lvl = 14; if (xpi > 15000) lvl = 15; if (xpi > 16000) lvl = 16;
+    if (xpi > 17000) lvl = 17; if (xpi > 18000) lvl = 18; if (xpi > 19000) lvl = 19; if (xpi > 20000) lvl = 20;
 
     
     let ambed = new Discord.RichEmbed()
