0549c1cf8206364ad39901564e1d2f187e806cc6
12321312 <xshadownightx12@gmail.com>
2019-07-04 19:10:59 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message,args,connection) => {
     connection.query(`SELECT * FROM xp WHERE id = '${a.id}'`, (err, rows) => {
      if(err) throw err;
      let xpi = rows[0].xp;
-     //let point = rows[0].point;
+     let point = rows[0].point;
     
     var lvl = 1;
     if (xpi > 1000) lvl = 1; if (xpi > 2000) lvl = 2; if (xpi > 3000) lvl = 3; if (xpi > 4000) lvl = 4; if (xpi > 5000) lvl = 5; if (xpi > 7000) lvl = 7; if (xpi > 8000) lvl = 8; if (xpi > 9000) lvl = 9; if (xpi > 10000) lvl = 10; if (xpi > 11000) lvl = 11; if (xpi > 12000) lvl = 12; if (xpi > 13000) lvl = 13; if (xpi > 14000) lvl = 14; if (xpi > 15000) lvl = 15; if (xpi > 16000) lvl = 16;
@@ -21,7 +21,7 @@ module.exports.run = async (bot,message,args,connection) => {
     .addField("Дискриминатор",a.discriminator)
     .addField("Опыта:",xpi + " XP")
     .addField("Уровень:",lvl)
-    //.addField("Донат поинтов:",point)
+    .addField("Донат поинтов:",point)
     .addField("Создание аккаунта",a.createdAt)
     .setThumbnail(a.avatarURL);
 
