e83e4e73a096a15cc9c712caad281552ec79231d
12321312 <xshadownightx12@gmail.com>
2019-07-04 15:13:37 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -5,6 +5,9 @@ module.exports.run = async (bot,message,args,connection) => {
     connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
      if(err) throw err;
      let xp = rows[0].xp;
+
+    let lvl = xp.slice(0, -3);
+    if (1000 > xp) lvl = 1;
     let ambed = new Discord.RichEmbed()
     .setTitle("Информация о участнике")
     .setTimestamp()
@@ -13,6 +16,7 @@ module.exports.run = async (bot,message,args,connection) => {
     .addField("Тэг",target.tag)
     .addField("Дискриминатор",target.discriminator)
     .addField("Опыта:",xp + " XP")
+    .addField("Уровень:",lvl)
     .addField("Создание аккаунта",target.createdAt)
     .setThumbnail(target.avatarURL);
 
