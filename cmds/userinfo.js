e02ebb6cefd02722f33b17503a1a6c7b9092383e
12321312 <xshadownightx12@gmail.com>
2019-07-04 19:04:11 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message,args,connection) => {
     connection.query(`SELECT * FROM xp WHERE id = '${a.id}'`, (err, rows) => {
      if(err) throw err;
      let xpi = rows[0].xp;
-     let point = rows[0].point;
+     let point = rows[1].point;
     
     var lvl = 1;
     if (xp > 1000) lvl = 1; if (xp > 2000) lvl = 2; if (xp > 3000) lvl = 3; if (xp > 4000) lvl = 4; if (xp > 5000) lvl = 5; if (xp > 7000) lvl = 7; if (xp > 8000) lvl = 8; if (xp > 9000) lvl = 9; if (xp > 10000) lvl = 10; if (xp > 11000) lvl = 11; if (xp > 12000) lvl = 12; if (xp > 13000) lvl = 13; if (xp > 14000) lvl = 14; if (xp > 15000) lvl = 15; if (xp > 16000) lvl = 16;
