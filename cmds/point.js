cbd932266060b3329e017b78b8fc583f584d6acb
12321312 <xshadownightx12@gmail.com>
2019-07-04 18:04:05 +0700

    Create point.js

---
+++ cmds/point.js
@@ -0,0 +1,26 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args,connection) => {
+    let target = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
+    if(!target) return message.reply("такого участника нету");
+
+    connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
+    if(err) throw err;
+    let sql;
+    if(rows.length < 1) {
+    sql = `INSERT INTO xp (id, point) VALUES ('${target.id}', 0)`
+    } else {
+    let point = rows[0].point;
+    sql = `UPDATE xp SET point = ${xp} WHERE id = '${target.id}'`
+    }
+    connection.query(sql);
+    
+    if ((args[1]) == null) {
+      message.reply(`<@${target.id}> имеет на счету ` + point)
+    }
+
+});
+};
+module.exports.help = {
+    name: "поинт"
+};
