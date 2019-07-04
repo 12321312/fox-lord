2f18920ef6d3ad0339bd012dda3c8982e57338bd
12321312 <xshadownightx12@gmail.com>
2019-07-04 13:49:15 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -165,11 +165,11 @@ function handleDisconnect() {
         }                                    
       });  
       connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-          handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-          throw err;                                  // server variable configures this)
+        //console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+          handleDisconnect();                         
+        } else {                                      
+          throw err;                                  
         }
       });
     }
