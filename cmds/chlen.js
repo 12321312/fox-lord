b1095fefe4946fa97f44b89d5aa8534cee96017e
12321312 <xshadownightx12@gmail.com>
2019-07-06 03:38:07 +0700

    Update chlen.js

--- cmds/chlen.js
+++ cmds/chlen.js
@@ -1,6 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args,connection) => {
+connection.query(`SELECT * FROM clien WHERE id = '${message.author.id}'`, (err, rows) => { 
 if (message.member.roles.get('537707501819396098')) return message.reply('у девушек нет члена о_О');
 let randomclien = Math.floor(Math.random() * 30) + 1 ;
 if (message.author.id == "294844223675564034") randomclien = "999999999";
@@ -9,9 +10,8 @@ let nameclien = "Уууу блядь п@здец...";
 if (randomclien < 25) nameclien = "Ну перед пацанами уже можно выебнуться..."; 
 if (randomclien < 20) nameclien = "Ну сойдет чтобы похвастаться перед скромной тёлкой..."; 
 if (randomclien < 15) nameclien = "У вас пиздец маленький, советую не показывать девочкам, описаются от смеха...";
-connection.query(`SELECT * FROM clien WHERE id = '${message.author.id}'`, (err, rows) => { 
-    let cms = rows[0].cm;
-    if (cms > 0) return message.reply(`вы уже проходили тест, видно перезашли, я окажу услугу и выдам ваши **${cms} см** обратно`)
+let cms = rows[0].cm;
+if (cms > 0) return message.reply(`вы уже проходили тест, видно перезашли, я окажу услугу и выдам ваши **${cms} см** обратно`)
 
 let clien = new Discord.RichEmbed()
 .setTitle(message.author.username)
