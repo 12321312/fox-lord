c923f941b1f3ccabe490c91097e07d034e4332f5
12321312 <xshadownightx12@gmail.com>
2019-07-05 17:08:33 +0700

    Update canal.js

--- cmds/canal.js
+++ cmds/canal.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     let botmessage = args.join(" ");
     if (message.author.id !== "294844223675564034") return message.reply('Хитрожопых наказываю'); 
-    if ((args[0])==null) return message.reply('Пустота в обращении...');
+    if (!(args[0])) return message.reply('Пустота в обращении...');
     message.channel.send(botmessage);
     message.delete();
 };
