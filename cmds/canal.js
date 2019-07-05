2c29f1317c9e72b9c82441e72aff8adc4d91b441
12321312 <xshadownightx12@gmail.com>
2019-07-05 13:44:40 +0700

    Update canal.js

--- cmds/canal.js
+++ cmds/canal.js
@@ -2,8 +2,8 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     let botmessage = args.join(" ");
-    if (message.author.id !== "294844223675564034") { message.reply('Хитрожопых наказываю'); return; }
-    if ((args[0])==null) { message.delete(); message.reply('Пустота в обращении...'); return; }
+    if (message.author.id !== "294844223675564034") return message.reply('Хитрожопых наказываю'); 
+    if ((args[0])==null) return message.reply('Пустота в обращении...');
     message.channel.send(botmessage);
     message.delete();
 };
