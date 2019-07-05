d18df4756dd29e64feac12dc8e9b2b7cc39e9378
12321312 <xshadownightx12@gmail.com>
2019-07-05 20:59:01 +0700

    Update point.js

--- cmds/point.js
+++ cmds/point.js
@@ -6,9 +6,16 @@ module.exports.run = async (bot,message,args,connection) => {
     if(!target) return message.reply("такого участника нету");
     if (message.author.id !== "294844223675564034") return message.reply('Хитрожопых наказываю');
     
-    function points() {
-      let point = rows[0].point;
-      if (!(args[1])) {
+    connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
+    if(err) throw err;
+    let sql;
+    if(rows.length < 1) {
+    sql = `INSERT INTO xp (id, xp, point) VALUES ('${target.id}', 0, 0)`;
+    message.reply(`успешно записал пользователя <@${target.id}> в базу данных`);
+    } else {
+    let point = rows[0].point;
+    sql = `UPDATE xp SET point = ${point} WHERE id = '${target.id}'`
+    if (!(args[1])) {
         message.reply(`остаток баланса <@${target.id}> на данный момент: ` + `\`\`\`js\n${point}\`\`\``);
       } else {
           if(!(args[2])){         
@@ -25,18 +32,6 @@ module.exports.run = async (bot,message,args,connection) => {
             bot.send(`Снял пользователю <@${target.id}> - **${args[1]}** поинтов. \nОстаток баланса пользователя на данный момент: ` + `\`\`\`js\n${Number(point) - Number(args[1])}\`\`\``);
        } 
       };
-    }
-
-
-    connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
-    if(err) throw err;
-    let sql;
-    if(rows.length < 1) {
-    sql = `INSERT INTO xp (id, xp, point) VALUES ('${target.id}', 0, 0)`;
-    message.reply(`успешно записал пользователя <@${target.id}> в базу данных`);
-    points();
-    } else {
-    points();
     };
     connection.query(sql);
 
