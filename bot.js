4478a64582536176a04b435cf290417663e225d4
12321312 <xshadownightx12@gmail.com>
2019-07-05 22:37:27 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -251,8 +251,6 @@ bot.on('message', async message => {
   if(!message.member.roles.some(r=>["Лисий повелитель", "Куратор", "Дозорный", "Прислужник", "Music-key", "Nsfw-знаток", "Божество"].includes(r.name)) ){
     cooldown.add(message.author.id);
 }
-
-  if (message.Delete) return console.log('Удалено нахуй!');
   message.react(bot.emojis.get("554122910584012800"));
   let args = message.content.slice(prefix.length).trim().split(/ +/g);
   let command = args.shift().toLowerCase();
