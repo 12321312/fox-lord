5eed170bb622535c28eb4410980b57e51f5c92e7
12321312 <xshadownightx12@gmail.com>
2019-07-05 18:14:50 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -303,15 +303,6 @@ bot.on('message', (receivedMessage) => {
     }
     if (receivedMessage.content.indexOf(prefix) == 0) {
     receivedMessage.react(bot.emojis.get("554122910584012800"))
-    
-    if(cooldown.has(message.author.id)){
-        message.delete();
-        return message.reply("хэй! Подожди 7 секунд и пиши команду...")
-    }
-      if(message.member.roles.some(r=>["Лисий повелитель", "Куратор", "Дозорный", "Прислужник", "Music-key", "Nsfw-знаток", "Божество"].includes(r.name)) ){
-        cooldown.add(message.author.id);
-    }
-
     }
 });
 
