070eea9321f5e03f757b87776f67370c510af944
12321312 <xshadownightx12@gmail.com>
2019-07-05 22:15:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -251,12 +251,15 @@ bot.on('message', async message => {
   if(!message.member.roles.some(r=>["Лисий повелитель", "Куратор", "Дозорный", "Прислужник", "Music-key", "Nsfw-знаток", "Божество"].includes(r.name)) ){
     cooldown.add(message.author.id);
 }
-  message.react(bot.emojis.get("554122910584012800"));
   let args = message.content.slice(prefix.length).trim().split(/ +/g);
   let command = args.shift().toLowerCase();
   let cmd = bot.commands.get(command);
   if(cmd) cmd.run(bot,message,args,connection);
 
+  setTimeout(() => {
+      if (message) return;
+    message.react(bot.emojis.get("554122910584012800"));
+  }, 10000)
   setTimeout(() => {
     cooldown.delete(message.author.id)
   }, cdseconds * 1000)
