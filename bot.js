8476fac2463f2bd8330b9b99b51d87f96f90153d
12321312 <xshadownightx12@gmail.com>
2019-07-06 21:54:44 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ const embedThumbnail = true;
 const embedThumbnailLink = "http://pngimg.com/uploads/shield/shield_PNG1276.png"; 
 const mysql = require("mysql");
 const invites = {};
-const wait = require('util').promisify(setTimeout);
 let cooldown = new Set();
 let cdseconds = 7;
 
@@ -340,21 +339,19 @@ bot.on('ready', () => {
              type: "STREAMING"
          }
      });
-   wait(1000);
-   bot.guilds.forEach(g => {
-     g.fetchInvites().then(guildInvites => {
-       invites[g.id] = guildInvites;
-      });
-   });
 });
 
 // Автороль
 bot.on('guildMemberAdd', member => {
+    bot.guilds.forEach(g => {
+        g.fetchInvites().then(guildInvites => {
+            invites[g.id] = guildInvites;
   const ei = invites[member.guild.id];
-  ei = guildInvites;
+  invites[member.guild.id] = guildInvites;
   const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
   const inviter = bot.users.get(invite.inviter.id);
-  
+});
+});
   console.log('User ' + member.user.tag + ' зашёл на сервер!');
   let channel = bot.channels.get("537720268446236682");
   var role = member.guild.roles.get("537701217879588878");
