21ededbdfef9d002c06370bdec6532b87f474c3d
12321312 <xshadownightx12@gmail.com>
2019-07-07 00:35:52 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot,message,args,connection) => {
 
     if (lvl < lvl.toFixed(0)) lvl -= 1;
 
-    let roles = message.member.roles.map(r=> "<@&" + r.id + ">").join(', ').slice(24);
+    let roles = message.member.roles.filter(r => r.name !=="@everyone").map(r => r).join(',')
 
     let ambed = new Discord.RichEmbed()
     .setTitle("Информация о участнике")
