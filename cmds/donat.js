38556f5679f3c758bae53c4542f3d8f1e319d0e5
12321312 <xshadownightx12@gmail.com>
2019-07-06 16:05:34 +0700

    Update donat.js

--- cmds/donat.js
+++ cmds/donat.js
@@ -2,7 +2,23 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot,message,args,connection) => {
+  let ambed = new Discord.RichEmbed()
+    .setTitle("Донат меню:")
+    .setTimestamp()
+    .setFooter("Донат систем v1337", "https://img.lovepik.com/element/40057/9503.png_860.png")
+    .setColor('#FFFF00')
+    .addField("!донат","Вызывает это окно")
+    .addField("!донат <услуга> <кол-во>","Проводит операцию по покупке услуги.")
+    .addField("!юзеринфо","Показывает ваши поинты и другую важную информацию.")
+    .addField("Услуги:","**Член**\nПокупка сантиметров члена, 1 поинт - 1 см\n**Оринтация**\nМеняет местами тест роли 'Пидор/Натурал' - 20 поинтов")
+    .setThumbnail("http://pngimg.com/uploads/question_mark/question_mark_PNG73.png");
+  
+  if(!(arg[0])) return bot.send({embed:ambed});  
+
   connection.query(`SELECT * FROM xp WHERE id = '${message.author.id}'`, (err, rows) => {
+   
+
+
   if(err) throw err;
   let sql;
   let xp = rows[0].xp;
