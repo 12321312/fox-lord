a434d3d7e9f71dbe602c98ef0bea9303b9d8e7bf
12321312 <xshadownightx12@gmail.com>
2019-07-06 23:05:09 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -33,23 +33,23 @@ message.channel.bulkDelete(fetched)
    clearmess = new Discord.RichEmbed()
   .setDescription("Удалены сообщения")
   .setColor('#FFFFFF')
-  .setThumbnail("https://cdn1.iconfinder.com/data/icons/ui-controls-miscellaneous/32/chat-off-512.png")
+  .setThumbnail("https://api.icons8.com/download/62a92be89dfeaf8aad2ac897c989cae6875429c2/windows8/PNG/512/Industry/trash-512.png")
   .setFooter("Клеар месседжер v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
   .addField("Администратор:", `${message.author}`, true)
   .addField("Удаленного:", `<@${mention.id}>`, true)
   .addField("Канал:", message.channel, true)
   .addField("Удалено:", fetched.size, true);
-  } else {
+  } else if (!mention) {
    clearmess = new Discord.RichEmbed()
     .setDescription("Удалены сообщения")
     .setColor('#FFFFFF')
     .setThumbnail("https://cdn1.iconfinder.com/data/icons/ui-controls-miscellaneous/32/chat-off-512.png")
     .setFooter("Клеар месседжер v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
     .addField("Администратор:", `${message.author}`, true)
-    .addField("Удаленного:", `<@${mention.id}>`, true)
     .addField("Канал:", message.channel, true)
     .addField("Удалено:", fetched.size, true); 
   }
+
   console.log(fetched.size + ' сообщения пользователя ' + `${mention.user.tag}` + ' найдены, удаление...'); 
   message.channel.send('Удалено `' + fetched.size + '` сообщений пользователя ' + `<@${mention.id}>`);       
   message.channel.bulkDelete(fetched)
