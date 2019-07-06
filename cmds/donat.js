0561257cc8ffcde46641003990466bd14cb4062e
12321312 <xshadownightx12@gmail.com>
2019-07-06 15:48:40 +0700

    Create donat.js

---
+++ cmds/donat.js
@@ -0,0 +1,14 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot,message,args,connection) => {
+  connection.query(`SELECT * FROM xp,clien WHERE id = '${target.id}'`, (err, rows) => {
+
+  sql = `UPDATE xp SET xp = ${xp + 1000} WHERE id = '${message.author.id}'`
+
+  connection.query(sql);
+ });
+};
+module.exports.help = {
+    name: "донат"
+};
