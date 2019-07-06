83c230cfe9320f28eee6ed8db74aee062dbe17bd
12321312 <xshadownightx12@gmail.com>
2019-07-06 16:09:20 +0700

    Update donat.js

--- cmds/donat.js
+++ cmds/donat.js
@@ -5,13 +5,13 @@ module.exports.run = async (bot,message,args,connection) => {
   let ambed = new Discord.RichEmbed()
     .setTitle("Донат меню:")
     .setTimestamp()
-    .setFooter("Донат систем v1337", "https://img.lovepik.com/element/40057/9503.png_860.png")
+    .setFooter("Донат систем v1337", "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png")
     .setColor('#FFFF00')
     .addField("!донат","Вызывает это окно")
     .addField("!донат <услуга> <кол-во>","Проводит операцию по покупке услуги.")
     .addField("!юзеринфо","Показывает ваши поинты и другую важную информацию.")
-    .addField("Услуги:","**Член**\nПокупка сантиметров члена, 1 поинт - 1 см\n**Оринтация**\nМеняет местами тест роли 'Пидор/Натурал' - 20 поинтов")
-    .setThumbnail("http://pngimg.com/uploads/question_mark/question_mark_PNG73.png");
+    .addField("Услуги:","**Член**\nПокупка сантиметров члена, 1 поинт - 1 см\n**Оринтация**\nМеняет местами роли 'Пидор/Натурал' - 20 поинтов")
+    .setThumbnail("https://www.buybitcoinworldwide.com/img/goodicons/doublecoin.png");
   
   if(!(args[0])) return bot.send({embed:ambed});  
 
