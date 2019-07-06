934d2eba860e2c38066a7834495e42679a197a94
12321312 <xshadownightx12@gmail.com>
2019-07-07 00:54:57 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -16,7 +16,9 @@ module.exports.run = async (bot,message,args,connection) => {
 
     if (lvl < lvl.toFixed(0)) lvl -= 1;
 
-    let roles = message.member.roles.filter(r => r.name !=="@everyone").map(r => r).join(',')
+    var words = ['@Лисий повелитель', '@Куратор'];
+
+    let roles = message.member.roles.filter(r => r.name !== words).map(r => r).join(',')
 
     let ambed = new Discord.RichEmbed()
     .setTitle("Информация о участнике")
