2e40ebf4bb64f88411f9a41b52aa25c095bf93a4
12321312 <xshadownightx12@gmail.com>
2019-07-06 23:02:23 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -24,10 +24,36 @@ message.channel.bulkDelete(fetched)
   fetched = await message.channel.fetchMessages({limit: args[1]});
   fetched = fetched .filter(m => m.createdTimestamp >= Date.now() - 1179360000);
   if (mention) fetched = fetched.filter(m => m.author.id === mention.id || m.content === message.content);
+  
 
+  let mutechannel = message.guild.channels.get("537720268446236682");
+  if(!mutechannel) return message.channel.send("Сбились настройки логирования, проверьте пожалуйста их.");
+  let clearmess;
+  if (mention) {
+   clearmess = new Discord.RichEmbed()
+  .setDescription("Удалены сообщения")
+  .setColor('#FFFFFF')
+  .setThumbnail("https://cdn1.iconfinder.com/data/icons/ui-controls-miscellaneous/32/chat-off-512.png")
+  .setFooter("Клеар месседжер v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
+  .addField("Администратор:", `${message.author}`, true)
+  .addField("Удаленного:", `<@${mention.id}>`, true)
+  .addField("Канал:", message.channel, true)
+  .addField("Удалено:", fetched.size, true);
+  } else {
+   clearmess = new Discord.RichEmbed()
+    .setDescription("Удалены сообщения")
+    .setColor('#FFFFFF')
+    .setThumbnail("https://cdn1.iconfinder.com/data/icons/ui-controls-miscellaneous/32/chat-off-512.png")
+    .setFooter("Клеар месседжер v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
+    .addField("Администратор:", `${message.author}`, true)
+    .addField("Удаленного:", `<@${mention.id}>`, true)
+    .addField("Канал:", message.channel, true)
+    .addField("Удалено:", fetched.size, true); 
+  }
   console.log(fetched.size + ' сообщения пользователя ' + `${mention.user.tag}` + ' найдены, удаление...'); 
   message.channel.send('Удалено `' + fetched.size + '` сообщений пользователя ' + `<@${mention.id}>`);       
   message.channel.bulkDelete(fetched)
+  mutechannel.send({embed:clearmess})
   .catch(error => message.channel.send(`Error: ${error}`)); 
 };
 } else 
