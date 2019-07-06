d2f2afc875c5e42b7265bbb7a4eeaee49d2e8f7c
12321312 <xshadownightx12@gmail.com>
2019-07-06 16:50:17 +0700

    Update donat.js

--- cmds/donat.js
+++ cmds/donat.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot,message,args,connection) => {
   if(!(args[0])) return bot.send({embed:ambed});  
   
   if((args[0])) {
-  connection.query(`SELECT * FROM clien,xp WHERE clien.id = '${message.author.id}'`, (err, rows) => {
+  connection.query(`SELECT * FROM clien,xp WHERE clien.id = '${message.author.id}' and xp.id = '${message.author.id}'`, (err, rows) => {
    if(err) throw err;
    let cm = rows[0].cm; 
    let point = rows[0].point;    
