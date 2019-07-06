e978c8aa65a0880f903713d145b8f8b07b0c6e04
12321312 <xshadownightx12@gmail.com>
2019-07-07 06:49:56 +0700

    Update antispam.js

--- antispam.js
+++ antispam.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js"); // Requiring Library just for the sick of being there
+const Discord = require("discord.js"); 
 
 var authors = [];
 var warned = [];
@@ -6,32 +6,29 @@ var banned = [];
 var messageLog = [];
 
 module.exports = async (bot, options) => {
-  /* Option Definitions */
-  
-  const warnBuffer = (options && options.warnBuffer) || 3; // Default Value: 3
-  const maxBuffer = (options && options.maxBuffer) || 5; // Default Value: 5
-  const interval = (options && options.interval) || 1000; //Default Time: 2000MS (2 Seconds in Miliseconds)
-  const warningMessage = (options && options.warningMessage) || "please stop spamming!"; // Default Message: "please stop spamming!" (@User please stop spamming!)
-  const banMessage = (options && options.banMessage) || "has been hit by ban hammer for spamming!"; // Default Message: "has been hit by ban hammer for spamming!" (@User has been hit by ban hammer for spamming!)
-  const maxDuplicatesWarning = (options && options.maxDuplicatesWarning || 7); // Default Value: 7
-  const maxDuplicatesBan = (options && options. maxDuplicatesBan || 10); // Deafult Value: 7
-  const deleteMessagesAfterBanForPastDays = (options && options.deleteMessagesAfterBanForPastDays || 7); // Default Value: 10
-  const exemptRoles = (options && options.exemptRoles) || []; // Default Value: Nothingness (None)
-  const exemptUsers = (options && options.exemptUsers) || []; // Default Value: Nothingness (None)
-  
-  /* Make sure all variables have correct types */
-  // TO DO: Terminate process when one of these errors is runned.
 
-  if(isNaN(warnBuffer)) throw new Error("warnBuffer must be a number.");
-  if(isNaN(maxBuffer)) throw new Error("maxBuffer must be a number.");
-  if(isNaN(interval)) throw new Error("interval must be a number.");
-  if(!isNaN(banMessage) || banMessage.length < 5) throw new Error("banMessage must be a string and have at least 5 charcters length.");
-  if(!isNaN(warningMessage) || warningMessage.length < 5) throw new Error("warningMessage must be a string and have at least 5 characters.");
-  if(isNaN(maxDuplicatesWarning)) throw new Error("maxDuplicatesWarning must be a number.")
-  if(isNaN(maxDuplicatesBan)) throw new Error("maxDuplicatesBan must be a number.");
-  if(isNaN(deleteMessagesAfterBanForPastDays)) throw new Error("deleteMessagesAfterBanForPastDays must be a number.");
-  if(exemptRoles.constructor !== Array) throw new Error("extemptRoles must be an array.");
-  if(exemptUsers.constructor !== Array) throw new Error("exemptUsers must be an array.");
+  
+  const warnBuffer = 5; 
+  const maxBuffer =  8;
+  const interval = 1000;
+  const warningMessage = "хватит спамить!"; 
+  const banMessage = "был забанен за спам!"; 
+  const maxDuplicatesWarning = (7); 
+  const maxDuplicatesBan = (10); 
+  const deleteMessagesAfterBanForPastDays = (7); 
+  const exemptRoles = []; 
+  const exemptUsers = []; 
+
+  if(isNaN(warnBuffer)) throw new Error("Нету буфера для варна.");
+  if(isNaN(maxBuffer)) throw new Error("Нету буфера для мута.");
+  if(isNaN(interval)) throw new Error("Нету интервала.");
+  if(!isNaN(banMessage) || banMessage.length < 5) throw new Error("Нету сообщения для мута.");
+  if(!isNaN(warningMessage) || warningMessage.length < 5) throw new Error("Нету сообщения для предупреждения.");
+  if(isNaN(maxDuplicatesWarning)) throw new Error("Нету максимальных повторяющих сообщений для предупреждения.")
+  if(isNaN(maxDuplicatesBan)) throw new Error("Нету максимальных повторяющих сообщений для мут.");
+  if(isNaN(deleteMessagesAfterBanForPastDays)) throw new Error("Не указано сколько удалять.");
+  if(exemptRoles.constructor !== Array) throw new Error("Нету ролей, или аррея.");
+  if(exemptUsers.constructor !== Array) throw new Error("Нету пользователей, или аррея.");
   
   // Custom 'checkMessage' event that handles messages
  bot.on("checkMessage", async (message) => {
@@ -47,14 +44,15 @@ module.exports = async (bot, options) => {
       banned.push(m.author.id);
   
       let user = m.guild.members.get(m.author.id);
+      
       if (user) {
-        user.ban(deleteMessagesAfterBanForPastDays).then((member) => {
-          m.channel.send(`<@!${m.author.id}>, ${banMsg}`);
-          return true;
-       }).catch(() => {
-          m.channel.send(`Oops, seems like i don't have sufficient permissions to ban <@!${message.author.id}>!`);
-          return false;
-      });
+        let muteroleauto = message.guild.roles.find('name', "muted");  
+        user.addRole(muterole.id);
+
+        setTimeout(function(){
+            user.removeRole(muteroleauto.id);
+        }, 86400000);
+        return message.channel.send(`Замутил пользователя <@!${user.id}> на сутки, за неоднократный спам.`);
     }
   }
   
