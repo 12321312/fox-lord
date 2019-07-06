c6db6f6b8f4b5a09ac59a7eced3623fd40fdbbb2
12321312 <xshadownightx12@gmail.com>
2019-07-07 05:44:13 +0700

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,7 +1,8 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    message.reply('да нормальный у тебя пинг `' + `${message.createdTimestamp - Date.now()}` + '` м/с, успокойся');
+    message.delete(15000);
+    message.reply('да нормальный у тебя пинг `' + `${message.createdTimestamp - Date.now()}` + '` м/с, успокойся').then(async msg => await msg.delete(15000));
 };
 module.exports.help = {
     name: "пинг"
