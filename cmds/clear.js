6060600d182d5f6c53ecad272666c80e96d827c3
12321312 <xshadownightx12@gmail.com>
2019-07-06 23:10:08 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -9,11 +9,20 @@ message.delete();
 if (!(args[0])) return message.reply('А сколько удалять то? \n Напиши: `!удалить <число>`');
 if ((args[0]) >= 100) return message.reply('Больше 100 за раз не могу ;с');
 
+let clearmess = new Discord.RichEmbed()
+.setDescription("Удалены сообщения")
+.setColor('#FFFFFF')
+.setThumbnail("https://api.icons8.com/download/62a92be89dfeaf8aad2ac897c989cae6875429c2/windows8/PNG/512/Industry/trash-512.png")
+.setFooter("Клеар месседжер v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
+.addField("Администратор:", `${message.author}`, true)
+.addField("Канал:", message.channel, true)
+.addField("Удалено:", fetched.size, true);
 
 const fetched = await message.channel.fetchMessages({limit: args[0]});
 console.log(fetched.size + ' сообщения найдены, удаление...'); 
 message.channel.send('Удалено `' + fetched.size + '` сообщений');            
 message.channel.bulkDelete(fetched)
+mutechannel.send({embed:clearmess})
 .catch(error => message.channel.send(`Error: ${error}`)); 
 
 } else {
@@ -28,10 +37,8 @@ message.channel.bulkDelete(fetched)
 
   let mutechannel = message.guild.channels.get("537720268446236682");
   if(!mutechannel) return message.channel.send("Сбились настройки логирования, проверьте пожалуйста их.");
-  let clearmess;
-  if (mention) {
-   clearmess = new Discord.RichEmbed()
-  .setDescription("Удалены сообщения")
+  let clearmess = new Discord.RichEmbed()
+  .setDescription("Удалены сообщения участника")
   .setColor('#FFFFFF')
   .setThumbnail("https://api.icons8.com/download/62a92be89dfeaf8aad2ac897c989cae6875429c2/windows8/PNG/512/Industry/trash-512.png")
   .setFooter("Клеар месседжер v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
@@ -39,17 +46,7 @@ message.channel.bulkDelete(fetched)
   .addField("Удаленного:", `<@${mention.id}>`, true)
   .addField("Канал:", message.channel, true)
   .addField("Удалено:", fetched.size, true);
-  };
-  if (!mention) {
-   clearmess = new Discord.RichEmbed()
-    .setDescription("Удалены сообщения")
-    .setColor('#FFFFFF')
-    .setThumbnail("https://cdn1.iconfinder.com/data/icons/ui-controls-miscellaneous/32/chat-off-512.png")
-    .setFooter("Клеар месседжер v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
-    .addField("Администратор:", `${message.author}`, true)
-    .addField("Канал:", message.channel, true)
-    .addField("Удалено:", fetched.size, true); 
-  };
+
 
   console.log(fetched.size + ' сообщения пользователя ' + `${mention.user.tag}` + ' найдены, удаление...'); 
   message.channel.send('Удалено `' + fetched.size + '` сообщений пользователя ' + `<@${mention.id}>`);       
