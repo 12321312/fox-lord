16971cfb870ba7bcb80be26d05eb95f4b62b6088
12321312 <xshadownightx12@gmail.com>
2019-07-06 16:46:59 +0700

    Update donat.js

--- cmds/donat.js
+++ cmds/donat.js
@@ -16,14 +16,14 @@ module.exports.run = async (bot,message,args,connection) => {
   if(!(args[0])) return bot.send({embed:ambed});  
   
   if((args[0])) {
-  connection.query(`SELECT * FROM clien AS xp WHERE id = '${message.author.id}'`, (err, rows) => {
+  connection.query(`SELECT * FROM clien,xp WHERE clien.id = '${message.author.id}'`, (err, rows) => {
    if(err) throw err;
    let cm = rows[0].cm; 
    let point = rows[0].point;    
    let sql;
 
     if((args[0]) == "член") {
-     if (!(args[1])) return message.reply(`У вас на данный момент **${point} см**, если вы хотите купить еще см, то напишите так: \n*!донат член <кол-во>*`);
+     if (!(args[1])) return message.reply(`У вас на данный момент **${cm} см**, если вы хотите купить еще см, то напишите так: \n*!донат член <кол-во>*`);
       if((args[1]) > point) return message.reply(`У вас не хватает **${Number(args[1]) - Number(point)} поинта(ов)**, на данный момент ваш баланс **${point}**`);
     }
 
