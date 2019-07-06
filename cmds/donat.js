a9b5419c4c78f2c2a8d2a651d05e0292aa8387d7
12321312 <xshadownightx12@gmail.com>
2019-07-06 15:55:10 +0700

    Update donat.js

--- cmds/donat.js
+++ cmds/donat.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot,message,args,connection) => {
-  connection.query(`SELECT * FROM xp,clien WHERE id = '${message.author.id}'`, (err, rows) => {
+  connection.query(`SELECT * FROM xp WHERE id = '${message.author.id}'`, (err, rows) => {
   if(err) throw err;
   let sql;
   let xp = rows[0].xp;
