b5ba3d9f0b9ed0e8b3793a87a16d99ff4d39a239
12321312 <xshadownightx12@gmail.com>
2019-07-07 19:43:13 +0700

    Update admincommand.js

--- cmds/admincommand.js
+++ cmds/admincommand.js
@@ -10,7 +10,7 @@ let zhanei = message.member.roles.filter(r => r.name !=="@everyone" && r.name ==
     .setTimestamp()
     .setFooter("Твой милый бот", "https://cs4.pikabu.ru/post_img/big/2016/07/16/9/1468678258134342020.jpg")
     .setColor('#c10020')
-    setThumbnail("http://www.sclance.com/pngs/admin-png/admin_png_17235.jpg")
+    .setThumbnail("http://www.sclance.com/pngs/admin-png/admin_png_17235.jpg")
     .addField("!админка","Вызывает это окно");
     
     if(message.member.roles.some(r=>["Лисий повелитель", "Куратор", "Дозорный", "Прислужник"].includes(r.name))) ambed.addField("!мут *<юзер> <время(1s|1m|1h|1d|1w)> <причина>*","Запрещает писать в чат на указанное время");
