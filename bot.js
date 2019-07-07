be49fca5905c2543ebadca380f57dd5add464002
12321312 <xshadownightx12@gmail.com>
2019-07-07 23:16:03 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -400,7 +400,7 @@ console.log('User ' + member.user.tag + ' зашёл на сервер!');
 member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     invites[member.guild.id] = guildInvites;
-    const invite = guildInvites.find(i => console.log(ei.get(i.code)));
+    const invite = guildInvites.find(i => console.log(ei.get(i.code).uses));
     const inviter = bot.users.get(invite.inviter.id);
   let channel = bot.channels.get("537720268446236682");
   let esyy = bot.emojis.get("554122910584012800");
