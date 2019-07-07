10c59287614f169b0e57ef77d547929c889548f0
12321312 <xshadownightx12@gmail.com>
2019-07-07 16:46:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -395,7 +395,6 @@ bot.on('guildMemberAdd', member => {
 member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     invites[member.guild.id] = guildInvites;
-    if (!uses) uses = 1;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = bot.users.get(invite.inviter.id);
   console.log('User ' + member.user.tag + ' зашёл на сервер!');
