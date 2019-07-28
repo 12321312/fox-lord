3454fd1c774efdf3f9858353bb1d1b690f067dbc
12321312 <xshadownightx12@gmail.com>
2019-07-28 22:27:18 +0700

    Update info.js

--- cmds/info.js
+++ cmds/info.js
@@ -1,6 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
+
   let infore = new Discord.RichEmbed()
   .setDescription("Информация о сервере ~Fox's Shelter~")
   .setThumbnail(message.guild.iconURL)
@@ -9,7 +10,10 @@ module.exports.run = async (bot,message,args) => {
   .setColor("#FFDF00")
   .addField("Создатель бота и сервера:", `<@!294844223675564034>`)
   .addField("Всего пользователей на сервере:", message.guild.members.size, true)
-  .addField("Всего онлайн:", message.guild.members.filter(m => m.presence.status === 'online').size, true);
+  .addField("Всего онлайн:", message.guild.members.filter(m => m.presence.status === 'online').size, true)
+  .addField("Регион сервера:", message.guild.region, true)
+  .addField("Создан сервер:", message.guild.createdAt, true);
+
 
 
     message.channel.send({embed:infore}).then(async msg => await msg.delete(15000));
