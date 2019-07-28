d048295bcae0bc982c8b6935a45dc3dcd6b16d90
12321312 <xshadownightx12@gmail.com>
2019-07-28 20:58:30 +0700

    1

--- cmds/region.js
+++ cmds/region.js
@@ -4,15 +4,21 @@ module.exports.run = async (bot,message,args) => {
 if(!message.member.roles.some(r=>["Лисий повелитель", "Куратор", "Дозорный"].includes(r.name))) return message.reply('Отказано в доступе.');
 
 
-if ((args[0]) == "Россия" || "россия" || "рус" || "rus" || "russia") {
-message.guild.setRegion('Russia');
-message.reply('Поставлен регион "Россия".');
-} else if ((args[0]) == "Европа" || "европа" || "евро" || "Eur" || "eur") {
-message.guild.setRegion('Central Europe');
-message.reply('Поставлен регион "Центральная Европа".');
+if ((args[0]) == "Россия" || "россия" || "рус" || "rus" || "russia" || "Russia") {
+message.guild.setRegion('russia');
+message.reply('поставлен регион сервера: *"Россия"*.');
+return;
+} else if ((args[0]) == "Европа" || "европа" || "евро" || "Eur" || "eur" || "eu-central") {
+message.guild.setRegion('eu-central');
+message.reply('поставлен регион сервера: *"Центральная Европа"*.');
+return;
+} else if ((args[0]) == "japan" || "singapore" || "eu-central" || "india" || "us-central" || "london" || "eu-west" || "amsterdam" || "brazil" || "dubai" || "us-west" || "hongkong" || "us-south" || "southafrica" || "us-east" || "sydney" || "frankfurt" || "russia") {
+message.guild.setRegion(args[0]);
+message.reply(`поставлен регион сервера: *"${args[0]}"*.`);
+} else if (!(args[0])) {
+message.reply("укажите нужный вам регион: /nСписок: 'japan', 'singapore', 'eu-central', 'india', 'us-central', 'london', 'eu-west', 'amsterdam', 'brazil', 'dubai', 'us-west', 'hongkong', 'us-south', 'southafrica', 'us-east', 'sydney', 'frankfurt', 'russia'.");
+return;
 } else return message.reply('сервер не правильно указан.');
-
-
 };
 
 module.exports.help = {
