e18f46c5803e4ff249181d10b8f363a48c34db0b
12321312 <xshadownightx12@gmail.com>
2019-07-28 23:08:21 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const fs = require('fs');
 const ms = require("ms"); 
+bot.commands = new Discord.Collection();
 let config = require('./config.json');
 let prefix = config.prefix;
 const yourID = "294844223675564034"; 
@@ -131,32 +132,18 @@ function generateXp() {
 }
 
 // подключение
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
+fs.readdir('./cmds/',(err,files)=>{
+  if(err) console.log(err);
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+  if(jsfiles.length <=0) console.log("Нет комманд для загрузки!!");
+  console.log(`Загружено ${jsfiles.length} комманд`);
+  jsfiles.forEach((f,i) =>{
+      let props = require(`./cmds/${f}`);
+      console.log(`${i+1}.${f} Загружен!`);
+      bot.commands.set(props.help.name,props);
+  });
+});
 
-const loadCommands = module.exports.loadCommands = (dir = "./cmds/") => {
-    fs.readdir(dir, (error, files) => {                 
-        if (error) return console.log(error);                    
-
-        files.forEach((file) => {   
-            if (fs.lstatSync(dir + file).isDirectory()) {
-                loadCommands(dir + file + "/");
-                return;
-            }
-
-            delete require.cache[require.resolve(`${dir}${file}`)];
-
-            let props = require(`${dir}${file}`);
-
-            bot.commands.set(props.command.name, props);
-
-            if (props.command.aliases)  props.command.aliases.forEach(alias => { 
-                bot.aliases.set(alias, props.command.name); 
-            });
-        });
-    });
-};
-loadCommands();
 
 // проверка текста
 bot.on('message', async message => {
@@ -381,25 +368,9 @@ connection.query(`SELECT * FROM clien WHERE id = '${message.author.id}'`, (err, 
   await message.react(bot.emojis.get("554122910584012800"));
 
   let args = message.content.slice(prefix.length).trim().split(/ +/g);
-  let cmd = args.shift().toLowerCase();
-  let command;
-  
-  if (bot.commands.has(cmd)) {
-      command = bot.commands.get(cmd);
-  } else if (bot.aliases.has(cmd)) {
-      command = bot.commands.get(bot.aliases.get(cmd));
-  }
-  
-  if (!message.content.startsWith(prefix)) return;
-  
-  if (command) {
-     if (message.author.id !== "id_владельца_бота" && !command.command.enabled) return message.reply("извините. Команда была отключена!");
-  }
-  
-  try {
+  let command = args.shift().toLowerCase();
+  let cmd = bot.commands.get(command);
   if(cmd) cmd.run(bot,message,args,connection);
-  } catch (e) {
-  }
 
   setTimeout(() => {
     cooldown.delete(message.author.id)
