2f3e00e49fe24c4777e565807e39f20cb986887b
12321312 <xshadownightx12@gmail.com>
2019-07-29 00:25:02 +0700

    Update warn.js

--- cmds/warn.js
+++ cmds/warn.js
@@ -11,28 +11,44 @@ connection.query(`SELECT * FROM warn WHERE id = '${target.id}'`, (err, rows) => 
  if(err) throw err;
    let sql;
     if(rows.length < 1) {
-      if (!(args[1])) {
-      message.reply(`у пользователя <@${target.id}> нет варнов.`);    
-      } else {
+      if (!(args[1])) return message.reply(`у пользователя <@${target.id}> нет варнов.`);    
       sql = `INSERT INTO warn (id, one, onea, two, twoa, tri, tria) VALUES ('${target.id}', '${wReason}', '${message.author.id}', NULL, NULL, NULL, NULL)`;
-      message.reply(`Выдал варн <@${target.id}> с причиной "${wReason}" аминистратор <@${message.author.id}>`);
+      bot.send(`Выдал варн <@${target.id}> с причиной "${wReason}" аминистратор <@${message.author.id}>`);
       connection.query(sql);
-      };
+      message.delete();
      } else {
      let warn1 = rows[0].one;
      let warn1a = rows[0].onea;
      let warn2 = rows[0].two;  
      let warn2a = rows[0].twoa;
      let warn3 = rows[0].tri;
-     let warn3a = rows[0].tria;              
+     let warn3a = rows[0].tria;
+     
+
+     if (!(args[1])) {
+        let WarnEmbed = new Discord.RichEmbed()
+        .setTitle("Варны пользователя")
+        .setTimestamp()
+        .setThumbnail("https://pngimage.net/wp-content/uploads/2018/06/%D0%B1%D0%B0%D0%BD-png-3-300x200.png")
+        .setFooter("Варн систем v2000", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
+        .setColor("#bc0000")
+        .addField(`Первый варн от <@${warn1a}>`, `${warn1}`);
+        if (warn2) WarnEmbed.addField(`Второй варн от <@${warn2a}>`, `${warn2}`);
+        if (warn3) WarnEmbed.addField(`Третий варн от <@${warn2a}>`, `${warn2}`);
+
+         bot.send({embed:WarnEmbed});
+         return; 
+     }                   
       if (!warn2) {
       sql = `UPDATE warn SET two = '${wReason}', twoa = ${message.author.id} WHERE id = '${target.id}'`
-      message.reply(`Выдал второй варн <@${target.id}> с причиной "${wReason}" аминистратор <@${message.author.id}>`);
+      bot.send(`Выдал второй варн <@${target.id}> с причиной "${wReason}" аминистратор <@${message.author.id}>`);
       connection.query(sql);
+      message.delete();
       } else if (!warn3) {
         sql = `UPDATE warn SET tri = '${wReason}', tria = ${message.author.id} WHERE id = '${target.id}'`
-        message.reply(`Выдал третий варн <@${target.id}> с причиной "${wReason}" аминистратор <@${message.author.id}>`);
+        bot.send(`Выдал третий варн <@${target.id}> с причиной "${wReason}" аминистратор <@${message.author.id}>`);
         connection.query(sql);
+        message.delete();
       }   
      }
 });         
