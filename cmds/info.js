d5cebfe2d69ec4bfe75000c29917772143fb133d
12321312 <xshadownightx12@gmail.com>
2019-07-28 22:37:06 +0700

    Update info.js

--- cmds/info.js
+++ cmds/info.js
@@ -11,7 +11,8 @@ module.exports.run = async (bot,message,args) => {
   .addField("Создатель бота и сервера:", `<@!294844223675564034>`)
   .addField("Всего пользователей на сервере:", message.guild.members.size, true)
   .addField("Всего онлайн:", message.guild.members.filter(m => m.presence.status === 'online').size, true)
-  .addField("Пинг:", bot.ping , true)
+  .addField("Мой пинг:", bot.ping , true)
+  .addField("Твой пинг:", message.author.ping, true)
   .addField("ID сервера:", message.guild.id)
   .addField("Регион сервера:", message.guild.region)
   .addField("Создан сервер:", message.guild.createdAt, true);
