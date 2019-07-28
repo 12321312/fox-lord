5cf3a10d55fd27393da5fec0366c0954c164bd66
12321312 <xshadownightx12@gmail.com>
2019-07-28 22:16:55 +0700

    Update info.js

--- cmds/info.js
+++ cmds/info.js
@@ -7,14 +7,13 @@ module.exports.run = async (bot,message,args) => {
   .setFooter("Твой милый бот", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
   .setTimestamp()
   .setColor("#FFDF00")
-  .addField("Создатель бота и сервера:", `<@!294844223675564034}>`)
+  .addField("Создатель бота и сервера:", `<@!294844223675564034>`)
   .addField("Всего пользователей на сервере:", message.guild.members.size, true)
   .addField("Всего онлайн:", message.guild.members.filter(m => m.presence.status === 'online').size, true);
 
 
     message.channel.send({embed:infore}).then(async msg => await msg.delete(15000));
-
-      message.delete(15000);
+    message.delete(15000);
 };
 module.exports.help = {
     name: "серверинфо"
