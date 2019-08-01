81aa16bf62f1dfefd528e013bf69b0c920af8e5f
12321312 <xshadownightx12@gmail.com>
2019-08-02 02:31:14 +0700

    Update baba.js

--- cmds/baba.js
+++ cmds/baba.js
@@ -6,16 +6,17 @@ module.exports.run = async (bot,message,args) => {
  let tobaba = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
  if(!tobaba) return message.reply("такого участника нету");
  if(tobaba.roles.some(r=>["Лисий повелитель", "Куратор", "Дозорный", "Прислужник"].includes(r.name))) return message.reply('админам сиськи не нужны');
- message.delete();
  let baba = message.guild.roles.find('name', "Барышня");
 
  if((args[1]) == "снять" || (args[0]) == "Снять" || (args[0]) == "-" || (args[0]) == "забрать" || (args[0]) == "Забрать") {
+    message.delete();
     tobaba.removeRole(baba.id);
     message.channel.send(`Пользователю <@${tobaba.id}> были урезаны сиськи.`);
     return;
  };
 
  if(tobaba.roles.some(r=>["Барышня"].includes(r.name))) return message.reply('ебанутый шоль?');
+ message.delete();
  tobaba.addRole(baba.id);
  message.channel.send(`Пользователю <@${tobaba.id}> были дарованы сиськи.`);
 
