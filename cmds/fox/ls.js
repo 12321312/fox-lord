f8bc8ea263a4f7a988243a0455e15275017dea57
12321312 <xshadownightx12@gmail.com>
2019-08-09 08:30:11 +0700

    Update ls.js

--- cmds/fox/ls.js
+++ cmds/fox/ls.js
@@ -2,12 +2,12 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 exports.run = async (bot, message, args) => { 
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);  
-    let botmessage = args[1];
+    let botmessage = args.slice(1).join(" ");
     if (message.author.id !== "294844223675564034") return message.reply('Хитрожопых наказываю'); 
     message.delete(); 
     if (!(args[0]) && (args[1])) return message.reply('пусто везде бл...');
     if (!member) return  message.reply('Такого нету...');
-    if ((args[1])) return message.reply('Пустота в описании...');
+    if (!(args[1])) return message.reply('Пустота в описании...');
 
     member.sendMessage(botmessage);
 };
