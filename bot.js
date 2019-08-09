31b4856a7a87d79f4d53dc566bae504e575ecebb
12321312 <xshadownightx12@gmail.com>
2019-08-09 08:14:04 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,9 +150,9 @@ bot.aliases = new Discord.Collection();
 const loadCommands = module.exports.loadCommands = (dir = "./cmds/") => {
     fs.readdir(dir, (error, files) => {                 
         if (error) return console.log(error); 
+        let jsfiles = files.filter(f => f.split(".").pop() === "js");
         if(jsfiles.length <=0) console.log("Нет комманд для загрузки!!");
         console.log(`Загружено ${jsfiles.length} комманд`);                   
-        let jsfiles = files.filter(f => f.split(".").pop() === "js");
         files.forEach((file) => {   
             if (fs.lstatSync(dir + file).isDirectory()) {
                 loadCommands(dir + file + "/");
