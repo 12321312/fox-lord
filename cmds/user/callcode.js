4e24f3d0aa9200ed1ed11e36732e58737b381c02
12321312 <xshadownightx12@gmail.com>
2019-08-19 15:58:41 +0700

    Update callcode.js

--- cmds/user/callcode.js
+++ cmds/user/callcode.js
@@ -12,7 +12,7 @@ const options = {
 exports.run = async (bot, message, args) => { 
     rp(options)
     .then(($) => {
-      console.log($('#nuclearcodess').find('.centered').text());
+      console.log($('#nuclearcodess').find('.centeredGood').text());
     })
     .catch((err) => {
       console.log(err);
