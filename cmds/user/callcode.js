72bdf3a5c8bb1c725f54f5c32e86f348b6ec162e
12321312 <xshadownightx12@gmail.com>
2019-08-19 16:32:51 +0700

    Update callcode.js

--- cmds/user/callcode.js
+++ cmds/user/callcode.js
@@ -17,7 +17,18 @@ exports.run = async (bot, message, args) => {
       let brava = code.slice(138, -71);
       let charli = code.slice(146, -63);
       
-      message.channel.send(`Альфа: ${alfa} Браво: ${brava} Чарли: ${charli}`);
+      let a = message.author;
+      let ambed = new Discord.RichEmbed()
+      .setTitle("Коды запуска ракет:")
+      .setTimestamp()
+      .setFooter("Система запуска.", "https://vignette.wikia.nocookie.net/fallout/images/c/c2/Icon_Vault_76.png/revision/latest?cb=20181217214332&path-prefix=ru")
+      .setColor('#c10020')
+      .setThumbnail("https://vignette.wikia.nocookie.net/fallout/images/b/b2/Mini_nuke.png/revision/latest?cb=20110404234408")
+      .addField("Альфа:", alfa)
+      .addField("Браво:", brava)
+      .addField("Чарли:", charli);
+
+      message.channel.send({embed:ambed});
 
     })
     .catch((err) => {
