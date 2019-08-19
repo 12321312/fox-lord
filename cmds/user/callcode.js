95ee0bab4371f4f2a0e354b9de98a75cea926aa4
12321312 <xshadownightx12@gmail.com>
2019-08-19 15:46:25 +0700

    Update callcode.js

--- cmds/user/callcode.js
+++ cmds/user/callcode.js
@@ -1,19 +1,23 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
-var request = require('request');
-var cheerio = require('cheerio');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const options = {
+  uri: `https://nukacrypt.com/`,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
 
 exports.run = async (bot, message, args) => { 
-    request('https://nukacrypt.com/', function(err, resp, body) {
-            if (err)
-                    throw err;
-            $ = cheerio.load(body);
-            $('a').each(function() { 
-            lol = ($('#nuclearcodess').find('.text-align: center;').text());
-            console.log(lol);
-            }); 
+    rp(options)
+    .then(($) => {
+      console.log($);
+    })
+    .catch((err) => {
+      console.log(err);
     });
-};
+
 
 
 module.exports.command = {
