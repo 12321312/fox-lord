d10e04deaf53d06e0c99e1cb081f90b855c67d05
12321312 <xshadownightx12@gmail.com>
2019-08-27 13:19:06 +0700

    Update callcode.js

--- cmds/user/callcode.js
+++ cmds/user/callcode.js
@@ -30,10 +30,9 @@ exports.run = async (bot, message, args) => {
       .setTimestamp()
       .setFooter("Система запуска.", "https://vignette.wikia.nocookie.net/fallout/images/c/c2/Icon_Vault_76.png/revision/latest?cb=20181217214332&path-prefix=ru")
       .setColor('#c10020')
-      .setThumbnail("https://vignette.wikia.nocookie.net/fallout/images/b/b2/Mini_nuke.png/revision/latest?cb=20110404234408")
-      .addField("Альфа:", alfa, true)
-      .addField("Браво:", brava, true)
-      .addField("Чарли:", charli, true);
+      .addField("Альфа:", `**${alfa}**`, true)
+      .addField("Браво:", `**${brava}**`, true)
+      .addField("Чарли:", `**${charli}**`, true);
 
       message.channel.send({embed:ambed});    
     })
