bd0658ac9f6f9718ad123a7bc9aecb26b9183e62
12321312 <xshadownightx12@gmail.com>
2019-08-27 13:16:46 +0700

    Update callcode.js

--- cmds/user/callcode.js
+++ cmds/user/callcode.js
@@ -18,6 +18,10 @@ exports.run = async (bot, message, args) => {
       let alfa = mcode.slice(31, -40);
       let brava = mcode.slice(39, -32);
       let charli = mcode.slice(47, -24);
+
+      if (charli === "UNSOLVED") charli = "НЕТ ДАННЫХ";
+      if (brava === "UNSOLVED") brava = "НЕТ ДАННЫХ";
+      if (alfa === "UNSOLVED") alfa = "НЕТ ДАННЫХ";
       
       let a = message.author;
       let ambed = new Discord.RichEmbed()
@@ -27,9 +31,9 @@ exports.run = async (bot, message, args) => {
       .setFooter("Система запуска.", "https://vignette.wikia.nocookie.net/fallout/images/c/c2/Icon_Vault_76.png/revision/latest?cb=20181217214332&path-prefix=ru")
       .setColor('#c10020')
       .setThumbnail("https://vignette.wikia.nocookie.net/fallout/images/b/b2/Mini_nuke.png/revision/latest?cb=20110404234408")
-      .addField("Альфа:", alfa)
-      .addField("Браво:", brava)
-      .addField("Чарли:", charli);
+      .addField("Альфа:", alfa, true)
+      .addField("Браво:", brava, true)
+      .addField("Чарли:", charli, true);
 
       message.channel.send({embed:ambed});    
     })
